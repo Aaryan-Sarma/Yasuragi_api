099e18a63e46796fcfb844ff36b7777ce6c80b50
Aaryan Sarma Inguva <ai8680@srmist.edu.in>
2024-10-07 09:24:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "API is live!"
+
 @app.route('/api/schedule', methods=['POST'])
 def generate_schedule():
     # Get data from the request
